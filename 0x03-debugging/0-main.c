d367e6eaaa2912436d756115f61d148d6593f132
Peterkin Yanzu <yanzupeterkin@gmail.com>
2023-02-24 01:35:02 +0000

    Debug 1

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -5,9 +5,15 @@
  */
 int main(void)
 {
-	int i;
+	int n;
 
-	i = 98;
-	positive_or_negative(i);
-	return (0);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+if (n > 0)
+printf("%d is positive\n", n);
+else if (n == 0)
+printf("%d is zero\n", n);
+else
+printf("%d is negative\n", n);
+return (0);
 }

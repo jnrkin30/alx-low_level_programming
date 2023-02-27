c33a8e3e22b52a723bf2e93b3bc261b06751cf34
Peterkin Yanzu <yanzupeterkin@gmail.com>
2023-02-27 23:39:30 +0000

    function that copies the string

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,18 +1,33 @@
 #include "main.h"
-
 /**
- * char *_strcpy - a function that copies the string pointed to by src
- * @dest: copy to
- * @src: copy from
- * Return: string
+ * _strcpy - copie the string
+ * @dest: array that returns
+ * @src: the pointer that receive the string
+ *
+ * Return: the parameter dest.
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i = 0;
+	int n = 0;
+	int i;
 
-	for (; src[i] != '\0'; i++)
+	while (src[n] != '\0')
+	{
+		n++;
+	}
+
+	i = 0;
+	while (i < n && src[i] != '\0')
 	{
 		dest[i] = src[i];
+		i++;
+	}
+
+	i = i;
+	while (i <= n)
+	{
+		dest[i] = '\0';
+		i++;
 	}
 	return (dest);
 }

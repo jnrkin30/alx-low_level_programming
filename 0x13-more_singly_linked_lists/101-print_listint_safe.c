2884f66b76ac4828d2c992aa49e642dc40c2f1ce
Peterkin Yanzu <yanzupeterkin@gmail.com>
2023-07-05 02:43:58 +0000

    more_singly_linked_lists

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,26 +1,37 @@
 #include "lists.h"
 
 /**
- * print_listint_safe - prints a list
- * @head: address of pointer to first node
+ * print_listint_safe - print a list
+ * @head: linked list of type listint_t
  *
- * Return: address of head
+ * Return: number of nodes in list, exit with status 98 if function fails
  */
+
 size_t print_listint_safe(const listint_t *head)
 {
-	if (head)
-	{
-		printf("[%p] %d\n", (void *)head, head->n);
-		if (head->next < head)
-		{
-			return (1 + print_listint_safe(head->next));
-		}
-		else
-		{
-			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
-			return (1);
-		}
+	const listint_t *current, *runner;
+	size_t c_i, r_i;
 
+	current = head;
+	c_i = 0;
+
+	while (current != NULL)
+	{
+		runner = head;
+		for (r_i = 0; r_i < c_i; r_i++)
+		{
+			if (runner == current)
+			{
+				printf("-> [%p] %d\n", (void *)current, current->n);
+				return (c_i);
+			}
+
+			runner = runner->next;
+		}
+		printf("[%p] %d\n", (void *)current, current->n);
+		current = current->next;
+		c_i++;
 	}
-	return (0);
+
+	return (c_i);
 }
